60f822e7ba8c5952a41b74ac76c61503d73dee6a
kaynansn11@gmail.com <kaynansn11@gmail.com>
2023-06-16 20:21:13 -0300

    "changes"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-
-import './App.css';
-import Video from "./pages/Video"
+import "./App.css";
+import Video from "./pages/Video";
 
 function App() {
   return (
     <div className="App">
-      <div className= "app__videos">
-        <Video/>
-        <Video/>
-        <Video/>
+      <div className="app__videos">
+        <Video
+          likes={100}
+          messages={200}
+          shares={300}
+          name="Paulo"
+          description="Brecker o goleiro"
+          music="musica épica"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+        />
+        <Video
+          likes={100}
+          messages={200}
+          shares={300}
+          name="Cezar"
+          description="Olhando para a câmera"
+          music="encarando"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"
+        />
+        <Video
+          likes={100}
+          messages={200}
+          shares={300}
+          name="Pow"
+          description="Brecker o goleiro"
+          music="musica épica"
+        />
       </div>
-      <header className="App-header">
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
